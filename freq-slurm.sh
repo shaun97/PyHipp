992fb1359c43903a1dc4ffe0ea01c3118ed0805e
Shaun Teo <xshaun97@gmail.com>
2020-10-31 00:51:22 +0800

    fix

--- freq-slurm.sh
+++ freq-slurm.sh
@@ -5,11 +5,11 @@
 #SBATCH --time=1:00:00   # walltime
 #SBATCH --ntasks=1   # number of processor cores (i.e. tasks)
 #SBATCH --nodes=1   # number of nodes
-#SBATCH -J "rpllfp"   # job name
+#SBATCH -J "freq"   # job name
 
 ## /SBATCH -p general # partition (queue)
-#SBATCH -o rpllfp-slurm.%N.%j.out # STDOUT
-#SBATCH -e rpllfp-slurm.%N.%j.err # STDERR
+#SBATCH -o freq-slurm.%N.%j.out # STDOUT
+#SBATCH -e freq-slurm.%N.%j.err # STDERR
 
 # LOAD MODULES, INSERT CODE, AND RUN YOUR PROGRAMS HERE
 python -u -c "import PyHipp as pyh; import time; pyh.FreqSpectrum(saveLevel=1); 
--- fsall-slurm.sh
+++ fsall-slurm.sh
@@ -5,11 +5,11 @@
 #SBATCH --time=1:00:00   # walltime
 #SBATCH --ntasks=1   # number of processor cores (i.e. tasks)
 #SBATCH --nodes=1   # number of nodes
-#SBATCH -J "rpllfp"   # job name
+#SBATCH -J "fasll"   # job name
 
 ## /SBATCH -p general # partition (queue)
-#SBATCH -o rpllfp-slurm.%N.%j.out # STDOUT
-#SBATCH -e rpllfp-slurm.%N.%j.err # STDERR
+#SBATCH -o fsall-slurm.%N.%j.out # STDOUT
+#SBATCH -e fsall-slurm.%N.%j.err # STDERR
 
 # LOAD MODULES, INSERT CODE, AND RUN YOUR PROGRAMS HERE
 python -u -c "import PyHipp as pyh; import time; 
